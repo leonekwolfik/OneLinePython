f8617b523c9e5d44f67f4d9edf27daed82f3ed86
leonekwolfik <35110136+leonekwolfik@users.noreply.github.com>
2022-01-22 20:05:49 +0100

    chapter 5.10: Text updater.

---
+++ moje/5_10.py
@@ -0,0 +1,15 @@
+import re
+
+text = '''
+Anna Kowalska wyszła za mąż za Jana Nowaka.
+Dawna 'Anna Kowalska' nazywa się teraz Anna Nowak.
+Anna Kowalska zmieniła swoje poprzednie nazwisko 'Kowalska' na 'Nowak'.
+Siostra Anny, Janina Kowalska, nadal nosi swoje dotychczasowe nazwisko.
+'''
+
+updated_text = re.sub("Anna Kowalska(?|')", "Anna Nowak", text)
+
+print(updated_text)
+
+assert updated_text != text
+assert updated_text.startswith("Anna Nowak")
