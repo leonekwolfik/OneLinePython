d0f9753c80d6c72e19eb839345686264aba52207
leonekwolfik <35110136+leonekwolfik@users.noreply.github.com>
2022-01-22 20:06:27 +0100

    chapter 5.10: Fixed Text updater.

--- moje/5_10.py
+++ moje/5_10.py
@@ -7,7 +7,7 @@ Anna Kowalska zmieniła swoje poprzednie nazwisko 'Kowalska' na 'Nowak'.
 Siostra Anny, Janina Kowalska, nadal nosi swoje dotychczasowe nazwisko.
 '''
 
-updated_text = re.sub("Anna Kowalska(?|')", "Anna Nowak", text)
+updated_text = re.sub("Anna Kowalska(?!')", 'Anna Nowak', text)
 
 print(updated_text)
 
