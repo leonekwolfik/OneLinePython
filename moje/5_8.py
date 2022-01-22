6617330aabcc87a79726279c6f5c33c283b4d6f5
leonekwolfik <35110136+leonekwolfik@users.noreply.github.com>
2022-01-22 19:40:18 +0100

    chapter 5.8: Find words with sign duplicates in text.

---
+++ moje/5_8.py
@@ -0,0 +1,18 @@
+import re
+
+## Dane
+text = '''
+Jak lekko, żwawo skacze rtęć
+I rośnie słupek w termometrze —
+Trzydzieści siedem, kresek pięć!
+Ach, tak niewinnie się zaczyna!
+Tylko oddechy coraz prędsze,
+I pod oczami sine piętna...
+Jak po drabinie rtęć się wspina,
+Po kreskach — srebrna, obojętna...
+-- Jerzy Liebert, Pieśń o zagładzie, 1934
+'''
+
+duplicates = re.findall('([^\s]*(?P<x>[^\s])(?P=x)[^\s]*)', text)
+
+print(duplicates)
