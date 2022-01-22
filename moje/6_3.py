9d39a1c6d44f53e19e37397e7b779db961bc3467
leonekwolfik <35110136+leonekwolfik@users.noreply.github.com>
2022-01-22 22:18:20 +0100

    chapter 6.3: Added non-recursion factorial function.

--- moje/6_3.py
+++ moje/6_3.py
@@ -3,3 +3,17 @@ n = 5
 factorial = lambda n: n * factorial(n - 1) if n > 1 else 1
 
 print(factorial(n))
+
+
+def fact(n):
+    result = 1
+    for i in range(1,n+1):
+        result *= i
+    return result
+
+print(fact(5))
+
+
+for i in range(10):
+    assert factorial(i) == fact(i)
+    
