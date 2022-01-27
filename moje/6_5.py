ecdab3b953ec390340df1d5fe7606d8c7c66ae06
leonekwolfik <35110136+leonekwolfik@users.noreply.github.com>
2022-01-27 20:05:58 +0100

    chapter 6.5. Update

--- moje/6_5.py
+++ moje/6_5.py
@@ -7,6 +7,5 @@ ps = lambda s: reduce(lambda P, x: P + [subset | {x} for subset in P], s, [set()
 print(ps(s))
 
 
-print([subset | {5} for subset in [set([1,2,3,4])]])
-
-print([set()] + [{1} | {2}])
+# print([subset | {5} for subset in [set([1,2,3,4])]])
+# print([set()] + [{1} | {2}])
