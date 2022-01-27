1250b746f743646f35b1b061ac4b6008f521a102
leonekwolfik <35110136+leonekwolfik@users.noreply.github.com>
2022-01-27 20:17:42 +0100

    chapter 6.8: Added other Fibonacci method.

--- moje/6_8.py
+++ moje/6_8.py
@@ -3,5 +3,8 @@ from functools import reduce
 n = 10
 
 fibs = reduce(lambda x, _: x + [x[-2] + x[-1]], range(n - 2), [0, 1])
-
 print(fibs)
+
+x = [0, 1]
+fibs2 = x[0:2] + [x.append(x[-1] + x[-2]) or x[-1] for i in range(n-2)]
+print(fibs2)
